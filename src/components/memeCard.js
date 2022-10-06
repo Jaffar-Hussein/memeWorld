5d396478a578e4c269dba58ebb8e8bac689142a5
Jaffar-Hussein <jaffhussein@icloud.com>
2022-10-06 09:48:39 +0300

    ft:card-arranging text in card

--- src/components/memeCard.js
+++ src/components/memeCard.js
@@ -1,15 +1,16 @@
 import Card from 'react-bootstrap/Card';
-import '../App.css' 
+import '../App.css'
 
-function memeCard({memes}) {
+function memeCard({ memes }) {
   return (
     <Card style={{ width: '18rem' }} className="MemeCards shadow ">
       <Card.Img variant="top" fluid="md" src={memes.image_url} style={{ height: '50%' }} className="cardHolder" />
-      <Card.Body>
-        <Card.Title>{memes.name}</Card.Title>
+      <Card.Body className="cardText">
+        <Card.Title className=''>{memes.name}</Card.Title>
+        <em className=''>{memes.category}</em>
       </Card.Body>
     </Card>
   );
 }
 
-export default memeCard;
+export default memeCard
