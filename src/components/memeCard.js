f01d3ed69a2ded097db9267831db246334a2fd4a
Jaffar-Hussein <jaffhussein@icloud.com>
2022-10-06 10:11:39 +0300

    ft:favicon-added fav icon that toggles

--- src/components/memeCard.js
+++ src/components/memeCard.js
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import '../App.css'
+import  '../assets/fav.png';
 
-function memeCard({ memes }) {
+function MemeCard({ memes }) {
+  const [fav, setFav]= useState(false);
+
+  function onAddFav(){
+    setFav(fav=> !fav);
+    if(fav){
+
+    }
+  }
+
   return (
     <Card style={{ width: '18rem' }} className="MemeCards shadow ">
       <Card.Img variant="top" fluid="md" src={memes.image_url} style={{ height: '50%' }} className="cardHolder" />
       <Card.Body className="cardText">
         <Card.Title className=''>{memes.name}</Card.Title>
-        <em className=''>{memes.category}</em>
+       <div className='d-flex justify-content-between'>
+       <em className=''>{memes.category}</em>
+       
+        <a onClick={onAddFav}><img src={ fav ? require('../assets/fav.png') : require('../assets/outliine.png') }/></a>
+       </div>
       </Card.Body>
     </Card>
   );
 }
 
-export default memeCard
+export default MemeCard
